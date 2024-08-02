4b56b0fb4c4331e22b154010f0488634ed541664
parkjunhee321 <jhee.park@moyoplan.com>
2024-08-02 17:38:30 +0900

    feat: navbar menu 추가

---
+++ src/constants/navbar/index.ts
@@ -0,0 +1,14 @@
+import { NavbarProps } from "../../components/common/Navbar";
+
+export const NAVBAR_MENU: NavbarProps["tabs"] = [
+  {
+    type: "button",
+    color: "primary",
+    text: "이메일로 소식 받기",
+  },
+  {
+    type: "button",
+    color: "default",
+    text: "채용 바로가기",
+  },
+];
