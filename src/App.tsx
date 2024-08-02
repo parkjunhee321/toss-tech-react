d674b4218feb34f9f5b0884eba1eacbd40930e44
parkjunhee321 <jhee.park@moyoplan.com>
2024-08-02 14:49:46 +0900

    feat: 페이지 추가

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AppLayout } from './components/layouts';
+import Home from './pages/home';
+import Article from './pages/article';
+
+function App() {
+  return (
+    <AppLayout>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/article/:key' element={<Article />} />
+      </Routes>
+    </AppLayout>
+  );
+}
+
+export default App;
